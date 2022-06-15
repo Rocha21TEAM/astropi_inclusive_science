e87e13f6b62e0a2b4d4779e43e3365820855dbfb
Carlos MS <cmorsoc@gmail.com>
2022-06-15 15:23:48 +0100

    Code commented

--- Extra_Code/mfi3d.py
+++ Extra_Code/mfi3d.py
@@ -1,16 +1,20 @@
-# Program: MFI3D
-# Authors: Rocha21
-# Description: Plots the MFI on the 3d Earth's model
+# Program: MFI3D.py
+# Authors: Rocha21 Team
+# Description: Plots the MFI on The Earth's 3D model
 # Resource: https://stevendkay.wordpress.com/2009/10/12/scatter-plots-with-basemap-and-matplotlib/
 
+# Import libraries
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import matplotlib.cm as cm
 
+# Load csv data
 data = pd.read_csv('000 expanded_Node2_Izzy_astro_pi_datalog PARA MAPA.csv', delimiter='\t')
 
+# Select the Magnetic Field Intensity (mfi) column from csv data
+# and plot it along (LON, LAT) coodinates.
 x=data.loc[:,'LON'].values
 y=data.loc[:,'LAT'].values
 z=data.loc[:,'mfi'].values
@@ -21,9 +25,7 @@ m = Basemap(projection='ortho', lat_0=28, lon_0=-15, resolution='l')
 # From Equator
 # m = Basemap(projection='ortho', lat_0=0, lon_0=60, resolution='l')
 
-#m = Basemap(projection='ortho', lat_0=0, lon_0=165, resolution='l')
-
-
+# Set up the map options and plot it
 m.drawcoastlines()
 m.drawcountries()
 
--- Extra_Code/plot_tat_lon_mfi colormap.py
+++ Extra_Code/plot_tat_lon_mfi colormap.py
@@ -1,20 +1,23 @@
-# Script: plot_tat_lon_mfi colormap.py
-# Description: Plot a 2d map of MFI
-# Authors: Rocha21
+# Program: plot_tat_lon_mfi colormap.py
+# Authors: Rocha21 Team
+# Description: Plots a 2D map of MFI
 
-
+# Import libraries
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np 
 
+# Load csv data
+data = pd.read_csv('000 expanded_Node2_Izzy_astro_pi_datalog PARA MAPA.csv', delimiter='\t')
+
+# Set up the plot
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
-data = pd.read_csv('000 expanded_Node2_Izzy_astro_pi_datalog PARA MAPA.csv', delimiter='\t')
-
 scatter=ax1.scatter(data.loc[:,'LON'],data.loc[:,'LAT'],c=data.loc[:,'mfi'],vmin=np.min(data.loc[:,'mfi']), vmax=np.max(data.loc[:,'mfi']),s=2,cmap=plt.cm.jet)
 
+# Set limits
 ax1.set_xlim(-185, 185)
 ax1.set_ylim(-55, 55)
 
